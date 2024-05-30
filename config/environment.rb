fe3fa02129e41d15240d01453e300ba42e3c532f
EC2 Default User <ec2-user@ip-172-31-18-61.ap-northeast-1.compute.internal>
2024-05-30 08:23:27 +0000

    fix production mode

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,11 @@
 # Load the Rails application.
 require_relative "application"
-
-Rails.application.configure do
-
-  config.web_console.allowed_ips = []
-
+if Rails.env.development?
+  Rails.application.configure do
+    
+    config.web_console.allowed_ips = []
+  
+  end
 end
 # Initialize the Rails application.
 Rails.application.initialize!
